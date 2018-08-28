dec46c56bd34b94df03269251c00f3241a747243
Fredy Aristizabal <faristizabal10@gmail.com>
2018-08-27 22:47:52 -0500

    Until products images on API
    Clone Zulu Repository. Until products images on API, image "noproduct" and developer of ProductPage.xaml

---
+++ Sales.API/Controllers/ProductsController.cs
@@ -0,0 +1,120 @@
+﻿namespace Sales.API.Controllers
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+    using System.Linq;
+    using System.Net;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using System.Web.Http.Description;
+    using Common.Models;
+    using Domain.Models;
+
+    public class ProductsController : ApiController
+    {
+        private DataContext db = new DataContext();
+
+        // GET: api/Products
+        public IQueryable<Product> GetProducts()
+        {
+            return this.db.Products;
+        }
+
+        // GET: api/Products/5
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> GetProduct(int id)
+        {
+            var product = await this.db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.ProductId)
+            {
+                return BadRequest();
+            }
+
+            this.db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await this.db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> PostProduct(Product product)
+        {
+            product.IsAvailable = true;
+            product.PublishOn = DateTime.Now.ToUniversalTime();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            this.db.Products.Add(product);
+            await this.db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = product.ProductId }, product);
+        }
+
+        // DELETE: api/Products/5
+        [ResponseType(typeof(Product))]
+        public async Task<IHttpActionResult> DeleteProduct(int id)
+        {
+            Product product = await this.db.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            this.db.Products.Remove(product);
+            await this.db.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return this.db.Products.Count(e => e.ProductId == id) > 0;
+        }
+    }
+}
