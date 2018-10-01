16fa8661cad65afad2a5264a4c2af12917661470
Fredy Aristizabal <faristizabal10@gmail.com>
2018-09-30 22:25:36 -0500

    SQLite Part I
    - Adding sqlite-net-pcl nuget
    - Creation interface
    - Adding DataService class

--- Sales/Sales/ViewModels/LoginViewModel.cs
+++ Sales/Sales/ViewModels/LoginViewModel.cs
@@ -105,7 +105,7 @@
             Settings.IsRemembered = this.IsRemembered;
 
             MainViewModel.GetInstance().Products = new ProductsViewModel();
-            Application.Current.MainPage = new ProductsPage();
+            Application.Current.MainPage = new MasterPage();
 
             this.IsRunning = false;
             this.IsEnabled = true;
